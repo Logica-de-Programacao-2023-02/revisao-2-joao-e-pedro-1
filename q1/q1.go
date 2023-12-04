69ba030f5f547a7e6e7c5562f9c3d7183e31c78e
jpnunis <141031499+jpnunis@users.noreply.github.com>
2023-12-04 00:28:01 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -15,3 +15,38 @@ package q1
 func CanBuyFood(stock map[string]int, dogs, cats int) bool {
 	return false
 }
+package main
+
+import "fmt"
+
+func main() {
+    quantCachorros := 2
+    quantGatos := 3
+
+    estoque := map[string]int{
+        "dog":       2,
+        "cat":       2,
+        "universal": 2,
+    }
+
+    if quantCachorros > estoque["dog"] {
+        fmt.Println("Não há ração suficiente para os cachorros.")
+        return
+    }
+
+    if quantGatos > estoque["cat"] {
+        fmt.Println("Não há ração suficiente para os gatos.")
+        return
+    }
+
+    estoque["dog"] -= quantCachorros
+    estoque["cat"] -= quantGatos
+
+    quantRestante := quantCachorros + quantGatos
+    if quantRestante > estoque["universal"] {
+        fmt.Println("Não há ração universal suficiente para o restante dos animais.")
+        return
+    }
+
+    fmt.Println("Polycarp pode comprar comida suficiente para todos os seus animais.")
+}
